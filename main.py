5b08fc8773c167c0af209aeef85ba2ec13fd24c7
NeiraJ <johnny.neira@gmail.com>
2024-07-19 09:21:31 +0200

    feat: Ajout de  la fonction dice_roll

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import random
+
+
+def dice_roll() -> int:
+    """
+    Simule le jet d'un dé à 6 faces
+    """
+    return random.choice(range(1, 6))
+
+
 def main():
-    pass
+    ...
 
 
 if __name__ == '__main__':
     main()
+
+
