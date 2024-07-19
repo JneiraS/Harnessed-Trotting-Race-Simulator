7ddac1489b4aefeea3a357439ff25a3111ac7ec8
NeiraJ <johnny.neira@gmail.com>
2024-07-19 10:01:20 +0200

    feat: ajout de la fonction alteration_of_speed pour calculer l'altération de vitesse

--- main.py
+++ main.py
@@ -11,11 +11,32 @@ def dice_roll() -> int:
     return random.choice(range(1, 6))
 
 
+def alteration_of_speed(actual_speed: int) -> int:
+    """
+    Calcule l'altération de vitesse en fonction de la vitesse actuelle, d'un lancé de dé simulé par la
+    fonction dice_roll() et d'une matrice.
+    La matrice contient des valeurs numériques représentant l'altération de vitesse ainsi qu'une valeur
+    'DQ' qui représenter une disqualification.
+
+    :return:
+    """
+    sheet: list[list] = [
+        [1, 2, 3, 4, 5, 6],
+        [0, 1, 1, 1, 2, 2],
+        [0, 0, 1, 1, 1, 2],
+        [0, 0, 1, 1, 1, 2],
+        [-1, 0, 0, 1, 1, 1],
+        [-1, 0, 0, 0, 1, 1],
+        [-2, -1, 0, 0, 0, 1],
+        [-2, -1, 0, 0, 0, 'DQ'],
+    ]
+    dice: int = dice_roll()
+    return sheet[actual_speed + 1][dice - 1]
+
+
 def main():
     ...
 
 
 if __name__ == '__main__':
-    main()
-
-
+    alteration_of_speed()
