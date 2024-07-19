5237af75c0965dee8daad4e345745dc9cd05a4b0
NeiraJ <johnny.neira@gmail.com>
2024-07-19 11:00:37 +0200

    feat: ajout de la fonction game_conf pour configurer la course
    - Ajout de la fonction game_conf() pour permettre à l'utilisateur de saisir le nombre de chevaux et le type de course
    - Validation des entrées utilisateur pour s'assurer qu'elles sont numériques et dans les plages spécifiées
    - Retourne une liste de chevaux et le type de course si les entrées sont valides
    - Affiche un message d'avertissement en cas d'entrées non valides

--- main.py
+++ main.py
@@ -55,12 +55,41 @@ def horse_factory(number_of_horses: int):
     """
     Crée une liste de chevaux en utilisant la fonction horse.
     """
-    return [horse(n+1) for n in range(number_of_horses)]
+    return [horse(n + 1) for n in range(number_of_horses)]
+
+
+def game_conf() -> tuple[list[dict], int]:
+    """
+    Configure la course en demandant à l'utilisateur de saisir le nombre de chevaux et le type de course.
+
+    Les entrées sont validées pour s'assurer qu'elles sont numériques et dans les plages spécifiées :
+    - Le nombre de chevaux doit être entre 12 et 20 inclus.
+    - Le type de course doit être 3 (tiercé), 4 (quarté) ou 5 (quinté).
+
+    :return: Une liste de chevaux et le type de course si les entrées sont valides.
+    """
+    options: list = [
+        input("\nNombre de chevaux (12 à 20): "),
+        input("Type (tiercé (3), quarté(4), quinté(5): "),
+    ]
+
+    if (
+            options[0].isnumeric()  # Vérifie si le premier élément est un nombre.
+            and options[1].isnumeric()  # Vérifie si le deuxième élément est un nombre.
+            and 12 <= int(options[0]) <= 20  # Vérifie si le premier élément est entre 12 et 20 inclus.
+            and 3 <= int(options[1]) <= 5  # Vérifie si le deuxième élément est entre 3 et 5 inclus.
+    ):
+        return horse_factory(int(options[0])), int(options[1])
+
+    else:
+        print(
+            "Attention! vous devez saisir des entiers dans les conditions demandées")
 
 
 def main():
-    ...
+    horses, type_oof_race = game_conf()
+
 
 
 if __name__ == '__main__':
-    print(horse_factory(12))
+    main()
